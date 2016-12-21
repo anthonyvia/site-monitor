ea40c84c2c1e3560fbeedd4958e7065ea59dfe15
Anthony Via <anthv123@gmail.com>
2016-12-21 15:42:12 -0800

    Add site_checks table. Insert a site_check record for each site created.

--- api/db/shema.sql
+++ api/db/shema.sql
@@ -3,9 +3,14 @@ CREATE TABLE `sites`.`sites` (
   `id` varchar(36) NOT NULL,
   `name` varchar(45) NOT NULL,
   `url` varchar(45) NOT NULL,
+  `check_interval` INT NOT NULL,
   `created_date` datetime NOT NULL,
   `is_active` tinyint(1) NOT NULL DEFAULT '1',
   `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
 );
+CREATE TABLE `sites`.`site_checks` (
+  `site_id` VARCHAR(36) NOT NULL,
+  `check_result` INT NULL,
+  `check_time` INT NOT NULL);
--- api/lib/database.js
+++ api/lib/database.js
@@ -3,29 +3,38 @@ var uuid = require('node-uuid');
 var Site = require('./site');
 
 var options = {
-  host: 'mysql',
+  host: 'localhost',
   user: 'root',
   password: 'root',
-  database: 'sites'
+  database: 'sites',
+  multipleStatements: true
 };
 
 var pool = mysql.createPool(options);
 
 function createSite(name, url, cb) {
   var query = '\
-INSERT INTO sites (id, name, url, created_date, is_active, is_deleted) \
-VALUES (?, ?, ?, ?, ?, ?);';
+START TRANSACTION; \
+INSERT INTO sites (id, name, url, check_interval, created_date, is_active, is_deleted) \
+VALUES (?, ?, ?, ?, ?, ?, ?); \
+INSERT INTO site_checks (site_id, check_result, check_time) \
+VALUES (?, ?, ?); \
+COMMIT;';
 
   var id = uuid.v4();
   var createdDate = new Date();
 
   pool
-    .query(query, [id, name, url, createdDate, 1, 0])
-    .then(function (rows) {
-      return cb(null, new Site({ id: id, name: name, url: url, createdDate: createdDate }));
+    .query(query, [id, name, url, 30, createdDate, 1, 0, id, null, 12])
+    .then(function () {
+      return cb(null, new Site({ id: id, name: name, url: url, check_interval: 30, createdDate: createdDate }));
     })
     .catch(function (err) {
-      return cb(err);
+      pool
+        .query('ROLLBACK;')
+        .then(function () {
+          return cb(err);
+        });
     });
 }
 
